2349949cdc0681413e7d94115d8cf22b0c2afb8a
inmaytide <inmaytide@gmail.com>
2024-12-18 13:37:24 +0800

    :wrench: Code Optimization

--- src/main/java/com/inmaytide/orbit/authorization/AuthorizationLauncher.java
+++ src/main/java/com/inmaytide/orbit/authorization/AuthorizationLauncher.java
@@ -1,6 +1,7 @@
 package com.inmaytide.orbit.authorization;
 
 import com.inmaytide.exception.web.BadRequestException;
+import com.inmaytide.exception.web.PathNotFoundException;
 import com.inmaytide.exception.web.mapper.PredictableExceptionMapper;
 import com.inmaytide.exception.web.translator.PredictableExceptionTranslator;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @EnableCaching
-@SpringBootApplication(scanBasePackages = {"com.inmaytide.orbit.authorization", "com.inmaytide.orbit.commons"})
+@SpringBootApplication(scanBasePackages = {"com.inmaytide.orbit.authorization", "com.inmaytide.orbit.commons", "com.inmaytide.orbit.commons.service"})
 public class AuthorizationLauncher {
 
 	public static void main(String[] args) {
@@ -21,6 +23,7 @@ public class AuthorizationLauncher {
 	public PredictableExceptionTranslator predictableExceptionTranslator() {
 		PredictableExceptionMapper mapper = new PredictableExceptionMapper();
 		mapper.register(OAuth2AuthenticationException.class, BadRequestException.class);
+		mapper.register(NoResourceFoundException.class, PathNotFoundException.class);
 		return new PredictableExceptionTranslator(mapper);
 	}
 
---
+++ src/main/java/com/inmaytide/orbit/authorization/api/SystemUserProvider.java
@@ -0,0 +1,26 @@
+package com.inmaytide.orbit.authorization.api;
+
+import com.inmaytide.orbit.commons.business.SystemUserService;
+import com.inmaytide.orbit.commons.domain.SystemUser;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author inmaytide
+ * @since 2024/12/18
+ */
+@RestController
+@RequestMapping("/api/system-users")
+public class SystemUserProvider {
+
+    private final SystemUserService service;
+
+    public SystemUserProvider(SystemUserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("{id}")
+    public SystemUser get(@PathVariable Long id) {
+        return service.get(id);
+    }
+}
+
